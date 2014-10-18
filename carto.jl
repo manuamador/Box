1d055d76effc61d755ff56d14ed3cd693596994b
manuamador <manuamador@gmail.com>
2014-10-18 11:52:16 +0200

    Fixes

--- carto.jl
+++ carto.jl
@@ -1,4 +1,5 @@
 include("ImageCreatorlxlylz.jl")
+#include("ImageCreator.jl")
 include("dipole.jl")
 
 const c = 299792458.
@@ -9,17 +10,20 @@ const eps0 = 1/(mu0*c^2)
 L=0.5
 l=0.45
 h=0.45
-lossesx= 0.5
-lossesy= 0.998
-lossesz= 0.998
+lossesx= 0.95
+lossesy= 0.95
+lossesz= 0.95
 order=20
 
-const freq=1e9
+const freq=[50e6,225e6,1e9,2e9]
+#const freq=[1e9]
+nf=length(freq)
+
 
 ##dipole
 #dipole position
-X=0.01
-Y=l/2
+X=L/3
+Y=l/3
 Z=h/2
 tilt=0#pi/2-acos(sqrt(2/3));
 azimut=0#pi/4
@@ -27,11 +31,13 @@ phase=0
 #dipole moment
 #total time averaged radiated power P= 1 W dipole moment => |p|=sqrt(12πcP/µOω⁴)
 Pow=1
-amplitude=sqrt(12*pi*c*Pow/(mu0*(2*pi*freq)^4))
+amplitude=sqrt(12*pi*c*Pow/(mu0*(2*pi*freq[end])^4))
+
 
 
 #Images
 POS=IC(L,l,h,X,Y,Z,tilt,azimut,phase,amplitude,order,lossesx,lossesy,lossesz)
+#POS=IC(L,l,h,X,Y,Z,tilt,azimut,phase,amplitude,order)
 
 #using HDF5,JLD
 #@load "POS100.jld" POS
@@ -44,83 +50,53 @@ numberofimages=length(POS[:,1])#1+2*order+(2*order*(order+1)*(2*order+1))/3
 dx=0.01
 dy=0.01
 dz=0.01
-const  nx=int(L/dx)
-const  xmax=L
-const  ny=int(h/dy)
-const  ymax=l
-const  x=linspace(0,L,nx)
-const  z=h/2
-const  y=linspace(0,l,ny)
+const  xmin=0-2*dx
+const  xmax=L+2*dx
+const  ymin=0-2*dy
+const  ymax=l+2*dy
+const  x=[xmin:dx:xmax]
+const  y=[ymin:dy:ymax]
+const  z=h/3
 
-
-println("Computing the radiation...")
-Z=zeros(nx,ny)
-Ez=zeros(nx,ny)
-Bz=zeros(nx,ny)
-for i=1:nx
-  perc=round(i/nx*1000)/10
-  println("$perc %")
-  for j=1:ny
-    r=[x[i],y[j],z]
-    E=zeros(Complex128,3)
-    B=zeros(Complex128,3)
-    for m=1:numberofimages
-      p=vec(POS[m,1:3])
-      R=vec(POS[m,4:6])
-      ord=POS[m,7]
-      Ed,Bd=Hertz_dipole (r, p, R, phase, freq)
-      E+=Ed
-      B+=Bd
-    end
-    Z[i,j]=sqrt(sum(abs(E).^2))/sqrt(sum(abs(B).^2))*mu0 #real(E).^2#0.5*numpy.cross(E.T,conjugate(B.T))
-    Ez[i,j]=abs(E[3])
-    Bz[i,j]=abs(B[3])
-  end
-end
+const nx=length(x)
+const ny=length(y)
 
 using PyPlot
-#pygui(false)
-
-figure(1)
-title("\$E_z\$ (V/m)")
-pcolor(x,y,Ez',cmap="jet")
-#clim(0,20)
-colorbar()
-axis("scaled")
-xlim(0,L)
-ylim(0,l)
-xlabel("\$x\$/m")
-ylabel("\$y\$/m")
-savefig("E_$order.png",bbox="tight")
-clf()
-
-
-figure(2)
-title("\$H_z\$ (A/m)")
-pcolor(x,y,Bz'/mu0,cmap="jet")
-#clim(0,.2)
-colorbar()
-axis("scaled")
-xlim(0,L)
-ylim(0,l)
-xlabel("x/m")
-ylabel("\$y\$/m")
-savefig("H_$order.png",bbox="tight")
-clf()
-
-
-figure(3)
-title("\$Z\$ (\$ \\Omega\$)")
-pcolor(x,y,Z',cmap="jet")
-clim(0,1000)
-colorbar(ticks=[0,100,200,300,120*pi,500,60*pi^2,700,800,900,1000])
-
-axis("scaled")
-xlim(0,L)
-ylim(0,l)
-#xlabel("x/m")
-ylabel("\$y\$/m")
-savefig("Z_$order.png",bbox="tight")
-clf()
+println("Computing the radiation...")
+Et=zeros(4,nx,ny,3)
+for k=1:nf
+  println("$k/$nf")
+  for i=1:nx
+    perc=round(i/nx*1000)/10
+    println("$perc %")
+    for j=1:ny
+      r=[x[i],y[j],z]
+      E=zeros(Complex128,3)
+      for m=1:numberofimages
+        p=vec(POS[m,1:3])
+        R=vec(POS[m,4:6])
+        ord=POS[m,7]
+        Ed,Bd=Hertz_dipole_ff (r, p, R, phase, freq[k])
+        E+=Ed
+      end
+      Et[k,i,j,:]=abs(E)
+    end
+  end
+  freqtxt=round(freq[k]/1e7)/100
+  figure(1)
+  title("\$E_z\$ (V/m), $freqtxt GHz")
+  pcolor(x,y,squeeze(Et[k,:,:,3],1)',cmap="jet")
+  #clim(0,20)
+  colorbar()
+  axis("scaled")
+  xlim(xmin,xmax)
+  ylim(ymin,ymax)
+  xlabel("\$x\$/m")
+  ylabel("\$y\$/m")
+  savefig("E_$k.png",bbox="tight")
+  clf()
+end
 
 
+using HDF5,JLD
+save("E_carto1.jld","Et",Et)
--- dipole.jl
+++ dipole.jl
@@ -22,7 +22,7 @@ function Hertz_dipole (r, p, R, phi, f, t=0, epsr=1.)
     rp_c_p_c_rp = cross(rprime_cross_p, rprime) # ((r-R) x p) x (r-R)
     expfac=exp(1im*(-w*t+krp-phi))/(4*pi*eps0*epsr)
     E=expfac*(w^2/(c^2*magrprime^3)*rp_c_p_c_rp+(1/magrprime^3-w*im/(c*magrprime^2))*(3*rprime*dot(rprime,p)/magrprime^2-p))
-	B=expfac/(magrprime*c^3)*(w^2*rprime_cross_p)/magrprime*(1-c/(im*w*magrprime))
+	  B=expfac/(magrprime*c^3)*(w^2*rprime_cross_p)/magrprime*(1-c/(im*w*magrprime))
     return E,B
   end
 
@@ -68,10 +68,10 @@ function Hertz_dipole_nf (r, p, R, phi, f, t=0, epsr=1.)
     w=2*pi*f  # omega
     k=w/c     # wave number
     krp=k*magrprime  # k*|r-R|
-	rprime_cross_p = cross(rprime, p) # (r-R) x p
+	  rprime_cross_p = cross(rprime, p) # (r-R) x p
     expfac=exp(1im*(-w*t+krp-phi))/(4*pi*eps0*epsr)
     E=expfac*((1/magrprime^3-w*im/(c*magrprime^2))*(3*rprime*dot(rprime,p)/magrprime^2-p))
-	B=expfac/(magrprime*c^2)*(w^2*rprime_cross_p)/magrprime*(-1/(im*w*magrprime))
+	  B=expfac/(magrprime*c^2)*(w^2*rprime_cross_p)/magrprime*(-1/(im*w*magrprime))
     return E,B
 	end
 
